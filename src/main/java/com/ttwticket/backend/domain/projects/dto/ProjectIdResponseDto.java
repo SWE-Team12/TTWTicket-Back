4904506e27fdf1ae2df15802489ded7d08dc7c1c
heejung <gimm9567@gmail.com>
2024-05-21 14:36:49 +0900

    feat: Project 생성시 사용되는 ResponseDto 작성

---
+++ src/main/java/com/ttwticket/backend/domain/projects/dto/ProjectIdResponseDto.java
@@ -0,0 +1,10 @@
+package com.ttwticket.backend.domain.projects.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProjectIdResponseDto {
+    private Integer projectId;
+}
