4ca0a47d86ee49ec4bfd174b4bb0fb6b839a9c86
heejung <gimm9567@gmail.com>
2024-05-21 14:38:33 +0900

    feat: ProjectService 추가

---
+++ src/main/java/com/ttwticket/backend/domain/projects/service/ProjectService.java
@@ -0,0 +1,22 @@
+package com.ttwticket.backend.domain.projects.service;
+
+import com.ttwticket.backend.domain.projects.ProjectRepository;
+import com.ttwticket.backend.domain.projects.dto.ProjectIdResponseDto;
+import com.ttwticket.backend.domain.projects.dto.ProjectRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+    @Transactional
+    public ProjectIdResponseDto createProject(ProjectRequestDto projectRequestDto) {
+        ProjectIdResponseDto projectIdResponseDto = ProjectIdResponseDto.builder()
+                .projectId(projectRepository.save(projectRequestDto.toEntity()).getProjectId())
+                .build();
+        return projectIdResponseDto;
+    }
+}
