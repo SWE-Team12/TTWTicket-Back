bd8697ef1c6719fae241bcb5c5d4c150dd23fa6c
StoneCAU <seokwoo1999@gmail.com>
2024-05-26 00:39:56 +0900

    유저 테스트코드(로그인,유효성,모든유저,이메일검색)

--- src/test/java/com/ttwticket/backend/domain/users/service/UserServiceTest.java
+++ src/test/java/com/ttwticket/backend/domain/users/service/UserServiceTest.java
@@ -1,10 +1,10 @@
 package com.ttwticket.backend.domain.users.service;
 
+import com.ttwticket.backend.domain.projects.dto.ProjectResponseDto;
 import com.ttwticket.backend.domain.users.Role;
 import com.ttwticket.backend.domain.users.User;
 import com.ttwticket.backend.domain.users.UserRepository;
-import com.ttwticket.backend.domain.users.dto.UserIdResponseDto;
-import com.ttwticket.backend.domain.users.dto.UserRequestDto;
+import com.ttwticket.backend.domain.users.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +13,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,7 +41,7 @@ class UserServiceTest {
 
     @Test
     @DisplayName("회원가입 성공")
-    void registerUser() {
+    void 회원가입() {
         //given
         UserRequestDto userRequestDto = UserRequestDto.builder()
                 .name("sname")
@@ -65,18 +70,106 @@ class UserServiceTest {
     }
 
     @Test
-    void login() {
+    @DisplayName("로그인 성공")
+    void 로그인() throws SQLException {
+        // given
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .name("sname")
+                .email("semail")
+                .password("spassword")
+                .role(Role.PL)
+                .build();
+
+        userService.registerUser(userRequestDto);
+
+        // when
+        UserLoginRequestDto userLoginRequestDto = UserLoginRequestDto.builder()
+                .email("semail")
+                .password("spassword")
+                .build();
+
+        UserLoginResponseDto userLoginResponseDto = userService.login(userLoginRequestDto);
+
+        // then
+        assertNotNull(userLoginResponseDto.getToken());
     }
 
     @Test
-    void getAllUsers() {
+    @DisplayName("모든 유저 조회 성공")
+    void 모든유저조회() {
+        // given
+        List<User> userList = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> User.builder()
+                        .name("test" + i)
+                        .email("test email" + i)
+                        .password("test password" + i)
+                        .role(Role.PL)
+                        .build())
+                .collect(Collectors.toList());
+        userRepository.saveAll(userList);
+
+        // when
+        List<UserResponseDto> userResponseDto = userService.getAllUsers();
+
+        // then
+        for (int i=0; i<userResponseDto.size(); i++) {
+            User user = userRepository.findByUserIdAndIsDeleted(userResponseDto.get(i).getUserId(), false);
+            assertEquals("test" + (i+1), userResponseDto.get(i).getName());
+            assertEquals("test email" + (i+1), userResponseDto.get(i).getEmail());
+            assertEquals("test password" + (i+1), userResponseDto.get(i).getPassword());
+            assertEquals(Role.PL, user.getRole());
+        }
+
+        assertEquals(userList.size(), userResponseDto.size());
     }
 
     @Test
-    void userValid() {
+    @DisplayName("유저 유효성 검사 성공")
+    void 유저유효성검사() throws SQLException {
+        // given
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .name("sname")
+                .email("semail")
+                .password("spassword")
+                .role(Role.PL)
+                .build();
+
+        userService.registerUser(userRequestDto);
+
+        // when
+        User user = userService.userValid("semail");
+
+        // then
+        assertNotNull(user);
+        assertEquals("sname", user.getName());
+        assertEquals("semail", user.getEmail());
+        assertThat(passwordEncoder.matches("spassword", user.getPassword())).isTrue();
+        assertEquals(Role.PL, user.getRole());
+        assertFalse(user.getIsDeleted());
+
     }
 
     @Test
-    void findUserByEmail() {
+    @DisplayName("이메일로 유저 찾기 성공")
+    void 이메일로유저찾기() throws SQLException {
+        //given
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .name("sname")
+                .email("semail")
+                .password("spassword")
+                .role(Role.PL)
+                .build();
+
+        userService.registerUser(userRequestDto);
+
+        //when
+        User user = userService.findUserByEmail("semail");
+
+        //then
+        assertNotNull(user);
+        assertEquals("sname", user.getName());
+        assertEquals("semail", user.getEmail());
+        assertTrue(passwordEncoder.matches("spassword", user.getPassword()));
+        assertEquals(Role.PL, user.getRole());
     }
 }
