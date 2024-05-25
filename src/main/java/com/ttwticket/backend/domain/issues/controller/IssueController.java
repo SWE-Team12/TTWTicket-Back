b4631f766fe57e2ad232670f4f914c6da028e550
heejung <gimm9567@gmail.com>
2024-05-26 01:52:33 +0900

    feat: issue 생성 기능 구현

--- src/main/java/com/ttwticket/backend/domain/issues/controller/IssueController.java
+++ src/main/java/com/ttwticket/backend/domain/issues/controller/IssueController.java
@@ -1,59 +1,56 @@
 package com.ttwticket.backend.domain.issues.controller;
 
-import com.ttwticket.backend.domain.issues.Status;
 import com.ttwticket.backend.domain.issues.dto.IssueIdResponseDto;
 import com.ttwticket.backend.domain.issues.dto.IssueRequestDto;
-import com.ttwticket.backend.domain.issues.dto.IssueResponseDto;
-import com.ttwticket.backend.domain.issues.dto.IssueStatusChangeRequestDto;
 import com.ttwticket.backend.domain.issues.service.IssueService;
-import com.ttwticket.backend.domain.users.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
+import java.sql.SQLException;
 
 @RestController
-@RequestMapping("/v1/project/{projectId}/issues")
+@RequestMapping("/v1/projects")
 @RequiredArgsConstructor
 @Slf4j
 public class IssueController {
 
     private final IssueService issueService;
 
-    @PostMapping("")
-    public IssueIdResponseDto create(@RequestBody IssueRequestDto issueRequestDto) {
-        return issueService.createIssue(issueRequestDto);
+    @PostMapping("/{projectId}/issues")
+    public IssueIdResponseDto create(@PathVariable("projectId") Integer projectId, @Validated @RequestBody IssueRequestDto issueRequestDto) throws SQLException {
+        return issueService.createIssue(issueRequestDto, projectId);
     }
 
-    @GetMapping("")
-    public List<IssueResponseDto> getAllIssues(@PathVariable("projectId") Integer projectId) {
-        return issueService.getAllIssues(projectId);
-    }
-
-    @GetMapping("/search")
-    public List<IssueResponseDto> searchIssues(@PathVariable("projectId") Integer projectId,
-                                               @RequestParam(value = "status", required = false) Status status,
-                                               @RequestParam(value = "fixer", required = false) User fixer,
-                                               @RequestParam(value = "reporter", required = false) User reporter) {
-        if (status != null) {
-            return issueService.getIssuesByStatus(projectId, status);
-        } else if (fixer != null) {
-            return issueService.getIssuesByUser(projectId, fixer);
-        } else if (reporter != null) {
-            return issueService.getIssuesByUser(projectId, reporter);
-        } else {
-            // Handle case when no query parameter is provided
-            // You can return an error response or handle it as per your application's requirements
-            return Collections.emptyList();
-        }
-    }
-
-    @PatchMapping("{issueId}")
-    public Integer modify(@PathVariable("issueId") Integer issueId, @RequestBody IssueStatusChangeRequestDto issueStatusChangeRequestDto) {
-        return issueService.modifyIssue(issueId, issueStatusChangeRequestDto);
-    }
+//    @GetMapping("/{projectId}/issues")
+//    public List<IssueResponseDto> getAllIssues(@PathVariable("projectId") Integer projectId) {
+//        return issueService.getAllIssues(projectId);
+//    }
+//
+//    @GetMapping("/{projectId}/issues/search")
+//    public List<IssueResponseDto> searchIssues(@PathVariable("projectId") Integer projectId,
+//                                               @RequestParam(value = "status", required = false) Status status,
+//                                               @RequestParam(value = "fixer", required = false) User fixer,
+//                                               @RequestParam(value = "reporter", required = false) User reporter) {
+//        if (status != null) {
+//            return issueService.getIssuesByStatus(projectId, status);
+//        } else if (fixer != null) {
+//            return issueService.getIssuesByUser(projectId, fixer);
+//        } else if (reporter != null) {
+//            return issueService.getIssuesByUser(projectId, reporter);
+//        } else {
+//            // Handle case when no query parameter is provided
+//            // You can return an error response or handle it as per your application's requirements
+//            return Collections.emptyList();
+//        }
+//    }
+//
+//    @PatchMapping("{issueId}")
+//    public Integer modify(@PathVariable("issueId") Integer issueId, @RequestBody IssueStatusChangeRequestDto issueStatusChangeRequestDto) {
+//        return issueService.modifyIssue(issueId, issueStatusChangeRequestDto);
+//    }
 
 
 }
--- src/main/java/com/ttwticket/backend/domain/issues/service/IssueService.java
+++ src/main/java/com/ttwticket/backend/domain/issues/service/IssueService.java
@@ -7,7 +7,10 @@ import com.ttwticket.backend.domain.issues.dto.IssueIdResponseDto;
 import com.ttwticket.backend.domain.issues.dto.IssueRequestDto;
 import com.ttwticket.backend.domain.issues.dto.IssueResponseDto;
 import com.ttwticket.backend.domain.issues.dto.IssueStatusChangeRequestDto;
+import com.ttwticket.backend.domain.projects.Project;
+import com.ttwticket.backend.domain.projects.ProjectRepository;
 import com.ttwticket.backend.domain.users.User;
+import com.ttwticket.backend.domain.users.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,41 +22,48 @@ import java.util.stream.Collectors;
 public class IssueService {
 
     private final IssueRepository issueRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
 
     @Transactional
-    public IssueIdResponseDto createIssue(IssueRequestDto issueRequestDto) {
+    public IssueIdResponseDto createIssue(IssueRequestDto issueRequestDto, Integer projectId) {
+        Project project = projectValid(projectId);
         IssueIdResponseDto issueIdResponseDto = IssueIdResponseDto.builder()
-                .issueId(issueRepository.save(issueRequestDto.toEntity()).getIssueId())
+                .issueId(issueRepository.save(issueRequestDto.toEntity(project)).getIssueId())
                 .build();
         return issueIdResponseDto;
     }
 
-    @Transactional
-    public Integer modifyIssue(Integer issueId, IssueStatusChangeRequestDto issueStatusChangeRequestDto) {
-        issueRepository.findByIssueId(issueId).modifyIssue(issueStatusChangeRequestDto);
-        return issueId;
-    }
+//    @Transactional
+//    public Integer modifyIssue(Integer issueId, IssueStatusChangeRequestDto issueStatusChangeRequestDto) {
+//        issueRepository.findByIssueId(issueId).modifyIssue(issueStatusChangeRequestDto);
+//        return issueId;
+//    }
+//
+//    public List<IssueResponseDto> getAllIssues(Integer projectId) {
+//        List<Issue> issues = issueRepository.findByProjectId(projectId);
+//        return issues.stream()
+//                .map(issue -> IssueResponseDto.builder().issue(issue).build())
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<IssueResponseDto> getIssuesByStatus(Integer projectId, Status status) {
+//        List<Issue> issues = issueRepository.findByProjectIdAndStatus(projectId, status);
+//        return issues.stream()
+//                .map(issue -> IssueResponseDto.builder().issue(issue).build())
+//                .collect(Collectors.toList());
+//    }
+//
+//
+//    public List<IssueResponseDto> getIssuesByUser(Integer projectId, User user) {
+//        List<Issue> issues = issueRepository.findByProjectIdAndFixer(projectId, user);
+//        return issues.stream()
+//                .map(issue -> IssueResponseDto.builder().issue(issue).build())
+//                .collect(Collectors.toList());
+//    }
 
-    public List<IssueResponseDto> getAllIssues(Integer projectId) {
-        List<Issue> issues = issueRepository.findByProjectId(projectId);
-        return issues.stream()
-                .map(issue -> IssueResponseDto.builder().issue(issue).build())
-                .collect(Collectors.toList());
-    }
-
-    public List<IssueResponseDto> getIssuesByStatus(Integer projectId, Status status) {
-        List<Issue> issues = issueRepository.findByProjectIdAndStatus(projectId, status);
-        return issues.stream()
-                .map(issue -> IssueResponseDto.builder().issue(issue).build())
-                .collect(Collectors.toList());
-    }
-
-
-    public List<IssueResponseDto> getIssuesByUser(Integer projectId, User user) {
-        List<Issue> issues = issueRepository.findByProjectIdAndFixer(projectId, user);
-        return issues.stream()
-                .map(issue -> IssueResponseDto.builder().issue(issue).build())
-                .collect(Collectors.toList());
+    public Project projectValid(Integer projectId) {
+        return projectRepository.findByProjectId(projectId);
     }
 
 
