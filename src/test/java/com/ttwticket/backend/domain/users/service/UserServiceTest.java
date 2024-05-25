04a1f88e3badcfd247d730abdd3163175ae0f55e
StoneCAU <seokwoo1999@gmail.com>
2024-05-26 01:04:34 +0900

    유저 테스트 코드 작성 끝

--- src/test/java/com/ttwticket/backend/domain/users/service/UserServiceTest.java
+++ src/test/java/com/ttwticket/backend/domain/users/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package com.ttwticket.backend.domain.users.service;
 
-import com.ttwticket.backend.domain.projects.dto.ProjectResponseDto;
 import com.ttwticket.backend.domain.users.Role;
 import com.ttwticket.backend.domain.users.User;
 import com.ttwticket.backend.domain.users.UserRepository;
@@ -9,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
