9d5759dd4556f0a06e4729d7c3148342d98dfca9
이석주 <sjlee0527@gmail.com>
2024-06-02 20:57:29 +0900

    assigneeService의 assignUser 메서드에서 이슈에 유저를 할당하고, 해당 이슈의 status를 assigned로 변경하도록 수정

--- src/main/java/com/ttwticket/backend/domain/assignees/service/AssigneeService.java
+++ src/main/java/com/ttwticket/backend/domain/assignees/service/AssigneeService.java
@@ -7,6 +7,9 @@ import com.ttwticket.backend.domain.assignees.dto.AssigneeRequestDto;
 import com.ttwticket.backend.domain.assignees.dto.AssigneeResponseDto;
 import com.ttwticket.backend.domain.issues.Issue;
 import com.ttwticket.backend.domain.issues.IssueRepository;
+import com.ttwticket.backend.domain.issues.Status;
+import com.ttwticket.backend.domain.issues.dto.IssueStatusChangeRequestDto;
+import com.ttwticket.backend.domain.issues.service.IssueService;
 import com.ttwticket.backend.domain.users.User;
 import com.ttwticket.backend.domain.users.UserRepository;
 import jakarta.transaction.Transactional;
@@ -24,11 +27,18 @@ public class AssigneeService {
     private final UserRepository userRepository;
     private final IssueRepository issueRepository;
     private final AssigneeRepository assigneeRepository;
+    private final IssueService issueService;
 
     @Transactional
     public AssigneeResponseDto assignUser(AssigneeRequestDto assigneeRequestDto, Integer issueId) {
         User user = userValid(assigneeRequestDto.getEmail());
         Issue issue = issueValid(issueId);
+
+        IssueStatusChangeRequestDto issueStatusChangeRequestDto = IssueStatusChangeRequestDto.builder()
+                .status(Status.assigned)
+                .build();
+
+        issueService.modifyIssue(issue.getProject().getProjectId(), issue.getIssueId(), issueStatusChangeRequestDto);
 
         AssigneeResponseDto assigneeResponseDto = AssigneeResponseDto.builder()
                 .assignee(assigneeRepository.save(new Assignee(user, issue)))
